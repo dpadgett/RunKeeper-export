0ec08bbad5ead00672669b0dad01732bd15e4efa
Dan Padgett <dumbledore3@gmail.com>
2020-05-15 04:18:52 +0000

    use right email

--- fetch_rk_activities.sh
+++ fetch_rk_activities.sh
@@ -14,6 +14,6 @@ activities=$(curl "https://fitnesskeeperapi.com/RunKeeper/deviceApi/getActivityU
 
 for activity in $activities; do
   echo $activity
-  curl "https://fitnesskeeperapi.com/RunKeeper/deviceApi/activities?maxWorkoutPaceVersion=2&utcOffsetSec=-25200&apiVer=2.3&timeZoneStr=America%2FLos_Angeles&deviceApp=paid%2C9.11.1.6494&device=android%2Csdk_gphone_x86%2C8.0.0%2C26%2CAndroid+SDK+built+for+x86&deviceID=e61a5b07-3709-4d4e-8b0c-6ba42d7d7033&email=dpadgett%40buffalo.edu&maxWorkoutUnitsVersion=0" -H "authorization: Bearer ${token}" -d "includePoints=true&cardioActivities=%5B%22${activity}%22%5D&includeStatusUpdates=true" > rk/${activity}.json
+  curl "https://fitnesskeeperapi.com/RunKeeper/deviceApi/activities?maxWorkoutPaceVersion=2&utcOffsetSec=-25200&apiVer=2.3&timeZoneStr=America%2FLos_Angeles&deviceApp=paid%2C9.11.1.6494&device=android%2Csdk_gphone_x86%2C8.0.0%2C26%2CAndroid+SDK+built+for+x86&deviceID=e61a5b07-3709-4d4e-8b0c-6ba42d7d7033&email=${email}&maxWorkoutUnitsVersion=0" -H "authorization: Bearer ${token}" -d "includePoints=true&cardioActivities=%5B%22${activity}%22%5D&includeStatusUpdates=true" > rk/${activity}.json
   ./rk_to_gpx.py "rk/${activity}.json" # || exit
 done
